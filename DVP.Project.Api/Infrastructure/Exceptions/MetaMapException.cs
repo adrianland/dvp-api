a2eaeccc7ef27d46a452a509ac1243d97f89ad85
adrianland <adrian5.3@hotmail.com>
2023-12-04 16:48:52 -0500

    push

--- DVP.Project.Api/Infrastructure/Exceptions/MetaMapException.cs
+++
@@ -1,8 +0,0 @@
-﻿using DVP.Project.Domain.Exception;
-
-namespace DVP.Project.Api.Infrastructure.Exceptions;
-
-public class MetaMapException : DVPException
-{
-    public MetaMapException(string message) : base(message) { }
-}
